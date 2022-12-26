3dfd8d5113315585e30aa6a5f6dc5622e304b419
sarukhadka1 <saru.khadka521@gmail.com>
2022-12-26 13:45:34 +0545

    login edited

--- src/view/login.java
+++ src/view/login.java
@@ -320,8 +320,7 @@ public class login extends javax.swing.JFrame {
         /*
          * If Nimbus (introduced in Java SE 6) is not available, stay with the default
          * look and feel.
-         * For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+        
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
