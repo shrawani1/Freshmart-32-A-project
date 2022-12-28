34031c52c8883a998b79753553898d7a3d394e8f
AakritiSedhai <secondshare007@gmail.com>
2022-12-28 23:18:13 +0545

    aboutUs

--- src/view/aboutUs.java
+++ src/view/aboutUs.java
@@ -53,7 +53,7 @@ public class aboutUs extends javax.swing.JFrame  {
         jPanel1.setBackground(new java.awt.Color(197, 234, 238));
 
         jLayeredPane1.setBackground(new java.awt.Color(255, 255, 255));
-        jLayeredPane1.setBorder(new javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED));
+        jLayeredPane1.setBorder( javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED));
         jLayeredPane1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLayeredPane2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
